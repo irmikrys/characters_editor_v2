c4b8ec5423191dd837771c1fce2f649e6fbb2d6f
Irmina Krysiak <irmina.krysiak@gmail.com>
2018-06-24 00:08:49 +0200

    [SOAP] Updating push.

--- soap-client/src/main/java/bean/ManagerBean.java
+++ soap-client/src/main/java/bean/ManagerBean.java
@@ -9,7 +9,7 @@ import org.soa.soap.editor.ElementDTO;
 import util.MessagesUtility;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.ApplicationScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
@@ -24,7 +24,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Named(value = "managerBean")
-@SessionScoped
+@ApplicationScoped
 public class ManagerBean implements Serializable {
 
     private static final long serialVersionUID = -7902579108607730057L;
@@ -155,6 +155,7 @@ public class ManagerBean implements Serializable {
                     elements
             );
         });
+        pushChannel.send("update");
     }
 
     private void addNode(TreeNode parentNode, String data, Collection<ElementDTO> elements) {
