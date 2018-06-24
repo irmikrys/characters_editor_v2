4aa84f7c1acc27a4d95b077c69727086ec16a1fc
Irmina Krysiak <irmina.krysiak@gmail.com>
2018-06-24 10:54:01 +0200

    Moving session bean functionality to catalog bean.

--- web/src/main/java/bean/CatalogBean.java
+++ web/src/main/java/bean/CatalogBean.java
@@ -1,23 +1,29 @@
 package bean;
 
 import bean.manager.PushBean;
+import bean.manager.SessionManagerBean;
 import boundary.CharactersServiceRemote;
 import model.Category;
 import model.Element;
 import model.TreeNodeData;
+import model.User;
 import org.primefaces.model.DefaultTreeNode;
 import org.primefaces.model.TreeNode;
 import util.EJBUtility;
 import util.MessagesUtility;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.naming.NamingException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.LinkedList;
@@ -30,9 +36,14 @@ public class CatalogBean implements Serializable {
     private static final long serialVersionUID = -9217712787886869451L;
 
     @Inject
+    private SessionManagerBean sessionManager;
+
+    @Inject
     private PushBean pushBean;
 
     private CharactersServiceRemote charactersServiceRemote;
+    private User userFromSession;
+
     private TreeNode root;
     private LinkedList<LinkedList<Element>> bestElements;
     private String errorMessage;
@@ -40,16 +51,71 @@ public class CatalogBean implements Serializable {
     public CatalogBean() throws NamingException {
 
         charactersServiceRemote = EJBUtility.lookupCharactersService();
-//        pushBean.dummyMethod();
         System.out.println("Catalog charactersServiceRemote constructor end");
     }
 
     @PostConstruct
     public void init() {
+        userFromSession = charactersServiceRemote.getUserFromSessionWithTypeSet();
+        String sessionMessage = sessionManager.getSessionMessage(userFromSession.getUsername());
+        pushBean.dummyMethod();
+        System.out.println("Initialization of session bean: " + sessionMessage);
+        if(sessionMessage.equals("Another session")) logout(true);
         initDataView();
         initBestElementsList();
         errorMessage = null;
     }
+
+    // ------------------ session ---------------------
+
+    @PreDestroy
+    public void destroy() {
+        sessionManager.logoutUser(userFromSession.getUsername());
+        System.out.println("Pre destroy: " + userFromSession.getUsername());
+    }
+
+    public String getCurrentUserTypeSetId() {
+        String id = Integer.toString(userFromSession.getTypeSet().getIdTypeSet());
+        System.out.println(id);
+        return id;
+    }
+
+    public User getUserFromSession() {
+        return userFromSession;
+    }
+
+    public void setUserFromSession(User userFromSession) {
+        this.userFromSession = userFromSession;
+    }
+
+    public void logout(boolean hasSession) {
+        HttpServletRequest req = (HttpServletRequest) (FacesContext.getCurrentInstance().getExternalContext().getRequest());
+        sessionManager.logoutUser(req.getRemoteUser());
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+
+        if(hasSession) {
+            facesContext.addMessage(
+                    null, new FacesMessage("You already have a session")
+            );
+        }
+
+        ExternalContext externalContext = facesContext.getExternalContext();
+        externalContext.getFlash().setKeepMessages(true);
+
+        try {
+            externalContext.redirect("index.xhtml");
+            facesContext.getMessageList().forEach(
+                    facesMessage -> System.out.println(facesMessage.getSummary())
+            );
+        } catch (IOException e) {
+            System.err.println("Redirect error");
+        }
+
+        externalContext.invalidateSession();
+
+    }
+
+    // ------------------------------------------------
 
     public void deleteElement(String type, Integer id) {
 
